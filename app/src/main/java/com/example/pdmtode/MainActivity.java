7081c24d82f7790fd262e09aa2e942f583d3c6ad
Savvas Mantzouranidis <mohito6@hotmail.com>
2021-04-13 12:36:53 +0300

    fb button, contact page, spinner
    Finished fb button action and look
    Fixed contact tab layout
    Set spinner items
    Cleaned and added comments to code

--- app/src/main/java/com/example/pdmtode/MainActivity.java
+++ app/src/main/java/com/example/pdmtode/MainActivity.java
@@ -1,4 +1,6 @@
 package com.example.pdmtode;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
@@ -32,8 +34,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Enable Internet Connection for RSS
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+
+        //Create Tab Layout
         TabLayout tabLayout = findViewById(R.id.tabs);
         TabItem tabNews = findViewById(R.id.announcements);
         TabItem tabProgram = findViewById(R.id.program);
@@ -49,20 +55,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
-
             }
-
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
+            public void onTabUnselected(TabLayout.Tab tab) {}
             @Override
-            public void onTabReselected(TabLayout.Tab tab) {
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
 
+        //Facebook Button
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.facebook.com/groups/ba.uowm/"));
+                startActivity(intent);
             }
         });
 
+        //Trigger RSS Read
         newsReader.read();
     }
 }
